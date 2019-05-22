dfc7167c5ee721c771ac5f9f51e4048f93e1a04e
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-22 23:13:39 +0200

    Update catch_mot.py
    j'en ai telllllement chié j'aitrop mangé

--- mot/catch_mot.py
+++ mot/catch_mot.py
@@ -1,75 +1,96 @@
 import os
+import requests
+import urllib.request
+from bs4 import *
 
-class Texte:
+from database import *
 
+
+
+class texte:
     def lecture(self, path):
-
-        self.path = path
-
-        liste_des_textes = []
-        
-        os.chdir(self.path)
+        os.chdir(path)
         liste = os.listdir()
+
+
+
+        liste1 = []
+
         for i in liste:
-            liste1 = []
             with open(i, 'r') as file:
                 liste1.append(file.read())
-            
-            liste_des_textes.append(liste1)
-            
-        return liste_des_textes
-            
-        
+
+     
+        return liste1
+
     def traitement_liste(self, liste):
         self.liste = liste
 
         liste_texte = []
 
-        for i in self.liste:
-            mot = ''
-            for j in i:
-                for k in j:
-                    if k == ' ':
+        for i in liste:
+            if i[-4:] == '.txt':
+                pass
+            else:
+                mot = ''
+                for j in i:
+                    if j == ' ':
                         liste_texte.append(mot)
                         mot = ''
                     else:
-                        mot += k
-
+                        mot += j
+                    
+     
         return liste_texte
 
 
-    def mot_liste(self, liste):
-        self.liste = liste
+
+    def recherche_mot_ou_database(self, texte):
+        self.texte = texte
+
+    
+
+        ranger = set(self.texte)
 
         dico = {}
-        
-        ranger = set(self.liste)
-
         for i in ranger:
             dico[i] = 0
-            
-        for i in self.liste:
+
+        for i in self.texte:
             for cle, valeur in dico.items():
                 if i == cle:
                     dico[i] += 1
 
-        print(dico)
-    
-    
+                    
+        for i in ranger:
+            
+            path = "https://www.larousse.fr/dictionnaires/francais/{}".format(i)
+          
+            r = requests.get(path)
 
-    def nombre_verbe():
-        pass
+            page = r.content
+            soup = BeautifulSoup(page, "html.parser")
+            propriete = soup.find("p", {'class':'CatgramDefinition'})
+
+            nombre = 0
+            for cle, valeur in dico.items():
+                if cle == i:
+                    nombre += valeur
 
 
-
-
-
-
-
-
-
-
-
+            print(i)
+            try:
+                print(i, propriete.string)
+                recherche = create_base.recherche_en_database(self, i)
+                #print(recherche)
+                if recherche == []:
+                    create_base.mis_en_database(self, i, str(propriete.string), nombre)
+                else:
+                    nouveau_nombre = int(recherche[0][3]) + nombre
+                    create_base.update_presence(self, i, nouveau_nombre)
+                    
+            except:
+                pass
 
 
 
