91f2aa443fa30ff78f1ba3610673f3ceb7feed1b
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-22 23:31:57 +0200

    Add files via upload
    en gros la t'enregistre juste les mots selon les textes faire la meme tout est expliqué suis a la lettre

---
+++ mot/devine_texte.py
@@ -0,0 +1,17 @@
+#continuer denregistrer des textes
+
+
+#puis dans ce fichier voir les mots qui reviennent et les fonctions qui reviennent
+
+#traiter les verbes dans un autre fichier
+
+
+#faire des courbes
+
+#enregistrer des textes tant que c pas significatif
+
+#quand ca le sera faut que ca fasse un truk qui dit triste ou joyeux
+
+#faire UNE TABLE MOT JOEYUX
+
+#et dis toi que mot == mot TRISTE
