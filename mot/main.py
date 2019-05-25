303a20224d3010271486eddd64a013e3f01f20d9
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-05-25 23:40:21 +0200

    Update main.py
    faudra mettre tous les sites

--- mot/main.py
+++ mot/main.py
@@ -8,6 +8,12 @@ if __name__ == '__main__':
 
     texte = texte()
     le_texte = texte.lecture(r'C:\Users\jeanbaptiste\Desktop\cluster\mot\texte\triste')
+
+    texte.nombre_ponctuation(le_texte)
     texte_mot = texte.traitement_liste(le_texte)
-    texte.recherche_mot_ou_database(texte_mot)
- 
+
+    verbe = texte.recherche_mot_ou_database(texte_mot)
+    texte.traitement_verbe(verbe)
+
+    ponctuation = texte.nombre_ponctuation(le_texte)
+    texte.traitement_ponctuation(ponctuation)
