97620c15ee82535c6c37b87aa10464b01d60b756
pastrouveedespeudo <46137867+pastrouveedespeudo@users.noreply.github.com>
2019-07-21 00:54:12 +0200

    Update database.py

--- momot/database.py
+++ momot/database.py
@@ -2,7 +2,9 @@ import psycopg2
 
 class database:
     
-    def creation_table(self):
+    def creation_table_principale(self):
+        """En gros on stock les verbe et les mots"""
+
 
         conn = psycopg2.connect(database='mot',
                                 user='postgres',
@@ -11,14 +13,54 @@ class database:
 
         cursor = conn.cursor()
         
-        cursor.execute("""create table momotv2(
+        cursor.execute("""create table boxx1(
                         id serial PRIMARY KEY,
                         mot varchar(100),
-                        presence INT);""")
+                        mot_fonction text,
+                        verbe varchar(100),
+                        verbe_fonction varchar(100),
+                        phrase text);""")
+
+        conn.commit()
+
+
+    def creation_table_librarie(self):
+        """en gros la faudra dire ouvrire image 1:ouverture lecture ect"""
+        conn = psycopg2.connect(database='mot',
+                                user='postgres',
+                                host='127.0.0.1',
+                                password='tiotiotio333')
+
+        cursor = conn.cursor()
+        
+        cursor.execute("""create table librarie(
+                        id serial PRIMARY KEY,
+                        mot varchar(100),
+                        librarie varchar(100));""")
 
         conn.commit()
 
 
 
-#database = database()
-#database.creation_table()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+database = database()
+database.creation_table_principale()
+database.creation_table_librarie()
